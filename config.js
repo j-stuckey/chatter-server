052c3d91134d1a479714a2ffac0eca259e6562da
James Stuckey <jstuckey@rutgers.edu>
2019-01-03 22:17:59 -0500

    added mongodb uri to config

--- config.js
+++ config.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/chatter',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/chatter',
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3d'
 };
